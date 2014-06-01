dcedd97fc743025e5737c9ccf8d855eb5837b650
Isocene <michael.reddock@gmail.com>
2014-06-01 18:35:38 -0500

    finally todos working

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,13 @@
 class TodosController < ApplicationController
 
+	def create
+		@list = List.find(params[:list_id])
+		@todo = @list.todos.create(todo_params)
+		redirect_to list_path(@list)
+	end
+
+	private
+		def todo_params
+			params.require(:todo).permit(:description)
+		end
 end
